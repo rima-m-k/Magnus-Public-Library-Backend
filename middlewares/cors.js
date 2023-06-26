4bb47992857263fcb1633789836fd58fcc461549
rima-m-k <rimamethalamkandy@gmail.com>
2023-06-26 17:06:50 +0530

    error while changing git repo

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,10 +1,10 @@
 const cors = require('cors');
 
 const corsMiddleware = cors({
-  // origin: 'https://magnuspubliclibrary.tech',
+  origin: 'https://magnuspubliclibrary.tech',
   // origin: 'http://localhost:3000',
-  // methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
-  // credentials: true,
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+  credentials: true,
 });
 
 module.exports = corsMiddleware;
